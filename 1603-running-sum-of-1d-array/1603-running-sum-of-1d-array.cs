7723883638dfc904226189914fb27e603c22a150
Manish Kumar <95469979+manish-tech-git@users.noreply.github.com>
2023-10-20 15:45:21 +0530

    Time: 128 ms (64.81%), Space: 43.2 MB (69.56%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+
+       int sum  = 0;
+       for (int i = 0; i < nums.Length; i++) {
+
+           sum = sum + nums[i];
+           nums[i] = sum;
+
+
+       }
+
+       return nums;
+        
+    }
+}
